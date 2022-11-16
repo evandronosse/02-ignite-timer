deac29e418592c7954c99c0649e41974fd4667a5
Evandro Nossé <evandronosse@chiptronic.com.br>
2022-11-16 17:41:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
+
+export default App
